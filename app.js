87dbe753079bde6aede3b4d19cafe8c9ae50a73b
@Jeremyobrien <jjob1391@gmail.com>
2021-11-05 16:38:13 -0400

    fixed static file delivery

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const data = require('./data.json');
-const projectData = data.projects;
-const path = require('path').posix;
+const { projects } = require('./data.json');
 const app = express();
 
 
@@ -9,43 +7,44 @@ const app = express();
 app.set("view engine", "pug");
 
 //static route
-app.use(express.static('public'));
+app.use('/static', express.static('public'));
 
 //homepage route
 app.get('/', (req, res) => {
-    res.locals = data.projects;
-    res. render('index');
+    res.render('index', { projects });
     res.end();
-})
+});
 
 app.get('/about', (req, res) =>{
-    // const err = new Error('Server Error');
-    // err.status = 500;
-    // next(err);
     res.render('about');
 })
 
 app.get('/project/:id', (req, res) => {
-    const requestedProject = projectData[req.params.id];
-    res.render('project', {projectName: requestedProject.project_name, description: requestedProject.description, technologies: requestedProject.technologies, liveLink: requestedProject.live_link, gitHubLink: requestedProject.github_link});
+    const requestedProject = projects[req.params.id];
+    if (requestedProject){
+        res.render('project', {projectName: requestedProject.project_name, description: requestedProject.description, technologies: requestedProject.technologies, liveLink: requestedProject.live_link, gitHubLink: requestedProject.github_link, imgUrls: requestedProject.image_urls});
+    } else {
+        next();
+    }
 })
 
 app.use((req, res, next)=>{
     const err =  new Error('Requested Page Not Found');
     err.status = 404;
+    console.log(`Error status ${err.status}, ${err.message}`);
     next(err);
 });
 
 app.use( (err, req, res, next) => {
-    // res.locals.error = err;
+    console.log(err)
     res.status(err.status || 500).send({
         error: {
             status: err.status || 500, 
-            message: err.message || 'Internal Server Error',
+            message: err.message || 'Internal Server Error. Please try again later.',
         },
     });
 });
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
+    console.log('The application is listening on localhost:3000');
 })
