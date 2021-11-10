cf7b65b38e7022c233af63bff4e09d62e7fbb878
@Jeremyobrien <jjob1391@gmail.com>
2021-11-10 16:01:55 -0500

    Added views and fixed project image delivery

--- app.js
+++ app.js
@@ -15,34 +15,39 @@ app.get('/', (req, res) => {
     res.end();
 });
 
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res, next) =>{
     res.render('about');
 })
 
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id', (req, res, next) => {
     const requestedProject = projects[req.params.id];
     if (requestedProject){
-        res.render('project', {projectName: requestedProject.project_name, description: requestedProject.description, technologies: requestedProject.technologies, liveLink: requestedProject.live_link, gitHubLink: requestedProject.github_link, imgUrls: requestedProject.image_urls});
-    } else {
-        next();
-    }
-})
+        res.render('project', { requestedProject });
+        } else {
+            next();
+        }
+    });
 
 app.use((req, res, next)=>{
-    const err =  new Error('Requested Page Not Found');
+    const err =  new Error('Requested Page Not Found. Please try again.');
     err.status = 404;
-    console.log(`Error status ${err.status}, ${err.message}`);
-    next(err);
+    if (err.status === 404) {
+        console.log(`Error status ${err.status}, ${err.message}`);
+        res.render('page-not-found', {err});
+    } else {
+        next(err);
+    } 
 });
 
 app.use( (err, req, res, next) => {
-    console.log(err)
-    res.status(err.status || 500).send({
-        error: {
-            status: err.status || 500, 
-            message: err.message || 'Internal Server Error. Please try again later.',
-        },
-    });
+    const error = err;
+    res.status(err.status || 500);
+    if (!err.message){
+        error.message = 'Internal Server Error. Please try again later.'
+    } else {
+        error.message =  err.message;
+    }
+    res.render('error', {error} );
 });
 
 app.listen(3000, () => {
